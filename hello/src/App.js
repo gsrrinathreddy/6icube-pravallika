08aa05b5e73ca4ceca9c6a174cf2450df1c355ea
Rani Pravallika <rajkiran.159@gmail.com>
2023-04-24 14:23:02 +0530

    hello adding

---
+++ hello/src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import Navbar from './Components/Navbar';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import React from 'react';
+import AutoComplete from './Components/Pages/AutoComplete';
+import Button from './Components/Pages/Button';
+import ButtonGroup from './Components/Pages/ButtonGroup';
+import CheckBox from './Components/Pages/CheckBox';
+import Products from './Components/Products';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/AutoComplete' element={<AutoComplete/>}/>
+        <Route path='/Button'element={<Button/>}/>
+        <Route path='/ButtonGroup'element={<ButtonGroup/>}/>
+        <Route path='/CheckBox'element={<CheckBox/>}/>
+        <Route path='/Products' elements={<Products/>}/>
+
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
